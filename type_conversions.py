bb6eb14ebb5c3d905b033e9ca95adc92df7c9f17
supriyagoud8 <113579979+supriyagoud8@users.noreply.github.com>
2022-09-19 15:07:15 +0530

    Add files via upload

---
+++ type_conversions.py
@@ -0,0 +1,22 @@
+# type conversion
+a=2
+print(a)
+print(float(a))#converting to float
+print(str(a))#converting to string
+
+b=3.7
+print(b)
+print(int(b))#converting into integer
+print(str(b))#converting into string
+print('b'+'hello')#concatenation
+
+c='8.9'
+print(float(c))
+n=float(c)#the string should be coverted into float first
+print(int(n))#8
+print(float(n))
+
+a=5
+b='heyy'
+c=7.9
+print(str(a)+b+str(c))#int and float into string
